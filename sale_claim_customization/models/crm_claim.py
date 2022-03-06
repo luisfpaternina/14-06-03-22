607d02ce18b921d5c7d1701800cc9d6ac80c281b
Bitsandresizquierdo <93657297+ingandresizquierdo@users.noreply.github.com>
2022-03-06 10:30:17 -0500

    campos

--- sale_claim_customization/models/crm_claim.py
+++ sale_claim_customization/models/crm_claim.py
@@ -5,6 +5,10 @@ _logger = logging.getLogger(__name__)
 
 class CrmClaim(models.Model):
     _inherit = "crm.claim"
+
+    reference = fields.Char(string="Ntra Reference", store=True)
+    reference_client = fields.Char(string="Client Reference", store=True)
+    solution = fields.Boolean('Solution')
   
 class ClaimLine(models.Model):
     _name = 'claim.line'
