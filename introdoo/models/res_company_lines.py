065fd0490c856e6b6b25f1ef948a90da58ffb122
Bitsandresizquierdo <93657297+ingandresizquierdo@users.noreply.github.com>
2022-03-06 09:37:51 -0500

    [DELETE] MODULO

--- introdoo/models/res_company_lines.py
+++
@@ -1,9 +0,0 @@
-
-from odoo import models, fields, api
-
-class ResCompanyLine(models.Model):
-    _name = 'res.company.line'
-    _description = 'company lines'
-
-    partner_id = fields.Many2one('res.partner', string="Contacto")
-    company_id = fields.Many2one('res.company',string="Compañia")
--- introdoo/models/res_config_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models, api, _
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    is_test = fields.Char(string="Test")
