d9c408927ca4584d0e32088446ef425e29b540eb
Bitsandresizquierdo <93657297+ingandresizquierdo@users.noreply.github.com>
2022-03-06 10:15:39 -0500

    a

--- sale_claim_customization/models/travel_sale.py
+++ sale_claim_customization/models/travel_sale.py
@@ -9,10 +9,5 @@ _logger = logging.getLogger(__name__)
 
 class TravelSale(models.Model):
     _inherit = 'travel_sale'
-
-    pickup_ids = fields.One2many('pickup_order', 'travel_id', string='Pickup Orders')
-    line_pickup_order_ids = fields.Many2many('pickup_order.line', related='pickup_ids.line_ids',
-                                             string="Articles with problems")
-
-
+    
 
