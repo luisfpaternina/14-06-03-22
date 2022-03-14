49e3cd2bff3b796097bca43d5c25176863c8c67f
Bitsandresizquierdo <93657297+ingandresizquierdo@users.noreply.github.com>
2022-03-14 11:28:20 -0500

    Update stock.py

--- sale_claim_customization/models/stock.py
+++ sale_claim_customization/models/stock.py
@@ -5,3 +5,6 @@ from odoo.exceptions import UserError
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
+
+
+    pickup_order_id = fields.Many2one('pickup.order', string="Pickup Orders", readonly=True, copy=False)
